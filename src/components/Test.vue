12cbe8fea9055929aacc15a0bc124040811fd58c
Aymannagyahmed <aymannagy.ahmed@gmail.com>
2023-12-02 20:36:31 +0200

    add tailwind theme

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="flex flex-wrap justify-center mt-10">
+  <div class="flex flex-wrap justify-evenly mt-10">
 
     <!-- card 1 -->
-    <div class="p-4 max-w-sm">
-      <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
-        <div class="flex items-center mb-3">
+    <div class="p-4 max-w-sm rounded overflow-hidden">
+      <!-- card image -->
+      <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-4 flex-col">
+        <div class="relative">
+        <a href="#">
+          <img class="w-full"
+            src="https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
+            alt="Sunset in the mountains">
+            <!-- overlay -->
           <div
-            class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-            </svg>
+            class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
           </div>
-          <h2 class="text-white dark:text-white text-lg font-medium">Feature 1</h2>
-        </div>
-        <div class="flex flex-col justify-between flex-grow">
+        </a>
+      </div>
+      
+        <div class="flex flex-col justify-between flex-grow mt-3">
           <p class="leading-relaxed text-base text-white dark:text-gray-300">
             Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.
           </p>
@@ -28,61 +31,6 @@
       </div>
     </div>
 
-    <!-- card 2 -->
-    <div class="p-4 max-w-sm">
-      <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
-        <div class="flex items-center mb-3">
-          <div
-            class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-            </svg>
-          </div>
-          <h2 class="text-white dark:text-white text-lg font-medium">Feature 2</h2>
-        </div>
-        <div class="flex flex-col justify-between flex-grow">
-          <p class="leading-relaxed text-base text-white dark:text-gray-300">
-            Lorem ipsum dolor sit amet. In quos laboriosam non neque eveniet 33 nihil molestias. Rem perspiciatis iure ut
-            laborum inventore et maxime amet.
-          </p>
-          <a href="#" class="mt-3 text-black dark:text-white hover:text-blue-600 inline-flex items-center">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
-
-    <!-- card 3 -->
-    <div class="p-4 max-w-sm">
-      <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
-        <div class="flex items-center mb-3">
-          <div
-            class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-            </svg>
-          </div>
-          <h2 class="text-white dark:text-white text-lg font-medium">Feature 3</h2>
-        </div>
-        <div class="flex flex-col justify-between flex-grow">
-          <p class="leading-relaxed text-base text-white dark:text-gray-300">
-            Lorem ipsum dolor sit amet. In quos laboriosam non neque eveniet 33 nihil molestias. Rem perspiciatis iure ut
-            laborum inventore et maxime amet.
-          </p>
-          <a href="#" class="mt-3 text-black dark:text-white hover:text-blue-600 inline-flex items-center">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
 
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      // padding: {
+      //   DEFAULT: "1rem",
+      //   sm: "2rem",
+      //   lg: "4rem",
+      //   xl: "5rem",
+      //   "2xl": "6rem",
+      // },
+    },
+    extend: {
+      aspectRatio: {
+        "4/3": "4 / 3",
+      },
+      columns: {
+        '4xs': '14rem',
+      }
+    },
   },
   plugins: [],
-}
+};
